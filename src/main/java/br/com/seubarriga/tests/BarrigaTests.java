61e89fffe86ececc89868846e28fda7c97509198
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-10 22:21:27 -0300

    Cenario de Testes - Deve alterar conta com sucesso

--- src/main/java/br/com/seubarriga/tests/BarrigaTests.java
+++ src/main/java/br/com/seubarriga/tests/BarrigaTests.java
@@ -1,15 +1,37 @@
 package br.com.seubarriga.tests;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.seubarriga.core.BaseTest;
 
 public class BarrigaTests extends BaseTest {
+	
+	private String token;
+	
+	@Before
+	public void login() {
+		//login na api
+		//receber token
+		Map<String, String> login = new HashMap<>();
+		login.put("email", "luis@daniel");
+		login.put("senha", "123456");
+		
+		this.token = given()
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token")
+		;
+	}
 
 	@Test
 	public void naoDeveAcessarSemToken() {
@@ -24,21 +46,6 @@ public class BarrigaTests extends BaseTest {
 	
 	@Test
 	public void deveIncluirContaComSucesso() {
-		//login na api
-		//receber token
-		Map<String, String> login = new HashMap<>();
-		login.put("email", "luis@daniel");
-		login.put("senha", "123456");
-		
-		String token = given()
-			.body(login)
-		.when()
-			.post("/signin")
-		.then()
-			.statusCode(200)
-			.extract().path("token")
-		;
-		
 		//Cadastro de conta
 		Map<String, String> conta = new HashMap<>();
 		conta.put("nome", "Conta para testes");
@@ -51,5 +58,25 @@ public class BarrigaTests extends BaseTest {
 		.then()
 			.statusCode(201);
 		;
+	}
+	
+	@Test
+	public void deveAlterarContaComSucesso() {
+		//Atualização da conta
+		Map<String, String> conta = new HashMap<>();
+		conta.put("nome", "Conta alterada para testes");
+		
+			
+		given()
+			.body(conta)
+			.header("Authorization", "JWT " + token)
+		.when()
+			.put("/contas/22091")
+		.then()
+			.statusCode(200)
+			.body("id", is(22091))
+			.body("nome", is("Conta alterada para testes"))
+	;
+		
 	}
 }
