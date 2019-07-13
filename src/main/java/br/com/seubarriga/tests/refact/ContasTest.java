5909eec9422f12ac9b23d22f49ae3bee072de445
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-12 22:28:26 -0300

    Teste de Contas

--- src/main/java/br/com/seubarriga/tests/refact/ContasTest.java
+++ src/main/java/br/com/seubarriga/tests/refact/ContasTest.java
@@ -1,11 +1,13 @@
 package br.com.seubarriga.tests.refact;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import br.com.seubarriga.core.BaseTest;
 import io.restassured.RestAssured;
@@ -34,4 +36,60 @@ public class ContasTest extends BaseTest {
 		//Reset da aplicacao
 		RestAssured.get("/reset").then().statusCode(200);
 	}
+	
+	@Test
+	public void deveIncluirContaComSucesso() {
+		//Cadastro de conta
+		Map<String, String> conta = new HashMap<>();
+		conta.put("nome", "Conta inserida");
+		
+		given()
+			.body(conta)
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(201)
+		;
+	}
+	
+	@Test
+	public void deveAlterarContaComSucesso() {
+		Integer contaId = this.getIdContaPeloNome("Conta para alterar");
+		
+		//Atualização da conta
+		Map<String, String> conta = new HashMap<>();
+		conta.put("nome", "Conta alterada");
+		
+			
+		given()
+			.body(conta)
+			.pathParam("id", contaId)
+		.when()
+			.put("/contas/{id}")
+		.then()
+			.statusCode(200)
+			.body("id", is(contaId))
+			.body("nome", is("Conta alterada"))
+		;	
+	}
+	
+	@Test
+	public void naoDeveIncluirContaComNomeRepetido() {
+		Map<String, String> conta = new HashMap<>();
+		conta.put("nome", "Conta mesmo nome");
+		
+		given()
+			.body(conta)
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(400)
+			.body("error", is("Já existe uma conta com esse nome!"))
+		;
+	}
+	
+	private Integer getIdContaPeloNome(String nome) {
+		return RestAssured.get("/contas?nome=" + nome).then().extract().path("id[0]");
+	}
+	
 }
